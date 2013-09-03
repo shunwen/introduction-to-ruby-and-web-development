a54e2dc12daf1f2718c96a84ed7cc9bec9228de9
shunwen <hsiaoshunwen@gmail.com>
2013-09-03 13:34:42 +0800

    Removed all global variables

--- week1_blackjack.rb
+++ week1_blackjack.rb
@@ -4,9 +4,6 @@ require 'pry'
 POINTS = {'ACE' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10}
 SUITS = ['C', 'D', 'H', 'S'] #["♣", "♦", "♥", "♠"]
 
-$player_hand = []
-$dealer_shoe = []
-
 def get_player_name name='Professor X'
   puts "Are you #{name} ? If not, please enter your name: "
   input = gets.chomp.capitalize
@@ -47,18 +44,18 @@ end
 def judge_player points
   case points
   when 1..20 then 'none'
-  when 21..21 then 'won'
+  when 21 then 'won'
   else 'busted'
   end
 end
 
-def act_player name
-  show_cards $player_hand, name
+def act_player name, hand, dealer_shoe
+  show_cards hand, name
 
-  action = nil
-  status = judge_player calculate_points $player_hand
+  status = judge_player calculate_points hand
 
   while status == 'none'
+    action = nil
     until ['hit', 'stay'].include? action
       print "Hit or stay? "
       action = gets.chomp.downcase
@@ -67,10 +64,9 @@ def act_player name
     if action == 'stay'
       break
     else
-      deal_card $player_hand, $dealer_shoe
-      show_cards $player_hand, name
-      status = judge_player calculate_points $player_hand
-      action = nil
+      deal_card hand, dealer_shoe
+      show_cards hand, name
+      status = judge_player calculate_points hand
     end
   end
 
@@ -89,15 +85,14 @@ def judge_dealer dealer_points, player_points
   player_result
 end
 
-def act_dealer hand
+def act_dealer hand, dealer_shoe, player_points
   show_cards hand
 
-  player_points = calculate_points $player_hand
   dealer_points = calculate_points hand
   player_result = judge_dealer dealer_points, player_points
 
   while dealer_points < 17 or dealer_points < player_points or player_result == 'none'
-    deal_card hand, $dealer_shoe
+    deal_card hand, dealer_shoe
     show_cards hand
     dealer_points = calculate_points hand
     player_result = judge_dealer dealer_points, player_points
@@ -123,19 +118,21 @@ def new_game? name
 
   action == 'n' ? false : true
 end
+
 # -- Blackjack
 
 player_name = get_player_name
 dealer_hand = []
+player_hand = []
 
 loop do
-  $dealer_shoe = get_dealer_shoe
-  init_hands [$player_hand, dealer_hand]
-  2.times { deal_card $player_hand, $dealer_shoe }
-  2.times { deal_card dealer_hand, $dealer_shoe }
+  dealer_shoe = get_dealer_shoe
+  init_hands [player_hand, dealer_hand]
+  2.times { deal_card player_hand, dealer_shoe }
+  2.times { deal_card dealer_hand, dealer_shoe }
 
-  result = act_player player_name
-  result = act_dealer dealer_hand if result == "dealer's turn"
+  result = act_player player_name, player_hand, dealer_shoe
+  result = act_dealer dealer_hand, dealer_shoe, calculate_points(player_hand) if result == "dealer's turn"
 
   case result
   when 'won' then player_won
