8e22ef5708eedd8da34630ff4b33acad38bdd466
shunwen <hsiaoshunwen@gmail.com>
2013-09-09 10:51:57 +0800

    Corrected indentation

--- week1_calculator.rb
+++ week1_calculator.rb
@@ -7,11 +7,10 @@ print "Choose an operator 1) add 2) sub 3) multiply 4) div : "
 operator = gets.chomp
 
 result = case operator.to_i
-  when 1 then oprand1.to_i + oprand2.to_i
-  when 2 then oprand1.to_i - oprand2.to_i
-  when 2 then oprand1.to_i * oprand2.to_i
-  else
-    oprand1.to_f / oprand2.to_f
+when 1 then oprand1.to_i + oprand2.to_i
+when 2 then oprand1.to_i - oprand2.to_i
+when 2 then oprand1.to_i * oprand2.to_i
+else oprand1.to_f / oprand2.to_f
 end
 
 puts "The answer is #{result}"
